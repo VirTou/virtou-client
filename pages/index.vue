b0911cb66c7375158468cbe6f37cf25de0b006a8
Pruthvi Shetty <pruthvishetty5656@gmail.com>
2020-09-20 01:31:48 +0530

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@
           rel="noopener noreferrer"
           class="button--green"
         >
-          Documentation
+          Docs
         </a>
         <a
           href="https://github.com/nuxt/nuxt.js"
